4a23dc20de2c0e4849a5ed73866401de8e02cfad
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-02-13 19:25:10 -0700

    Finished with Apply for Pets story

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,11 +1,20 @@
 class PetApplicationsController < ApplicationController
 
   def index
-    @applications = Application.all
+
   end
 
   def create
-
+    @pet = Pet.find(params[:pet_id])
+    @application = Application.find(params[:id])
+    @pet_applications = PetApplication.create(pet: @pet, application: @application)
+    if @pet_applications.save
+      flash[:success] = "Application successfully saved!"
+      redirect_to "/applications/#{@application.id}"
+    else
+      flash[:error] = "Application was not saved!"
+      render :back
+    end
   end
 
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,13 @@
 class Application < ApplicationRecord
-  has_many :pet_applications
-  has_many :pets, through: :pet_applications
+  has_many :pet_applications, :dependent => :destroy
+  has_many :pets, through: :pet_applications, :dependent => :destroy
   validates_presence_of :name, :address, :city, :state, :zip, :description, :status
+  after_initialize :default, unless: :persisted?
 
+
+  def default
+    self.status = "In Progress"
+    self.description = "<Please tell why you will be good for this pet>"
+  end
 
 end
