4533bf2824aea1d3fef5023994f69da35974a8a9
Khoa Nguyen <KhoaNg323@hotmail.com>
2021-02-15 18:03:58 -0700

    Finished approving and rejecting pets

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    root to: "admin#index"
-    resources :shelters, :applications
+    resources :applications, :shelters
   end
 
   get "/", to: "welcome#index"
